1230e407c8849e9e60f73c6950b742e5b31ace83
Jorge Hernán <jorge.hernan.esparza@gmail.com>
2019-07-17 18:56:40 +0200

    First commit

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends Controller
+{
+    /**
+     * @Route("/lucky/number")
+     */
+    public function numberAction()
+    {
+        $number = random_int(0, 100);
+        $em = $this->getDoctrine()->getManager();
+        $em->getConnection()->connect();
+        $connected = $em->getConnection()->isConnected();
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
