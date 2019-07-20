5fdc4608dc14f97e88372f77e2cedd3772e4749b
Jorge Hernán <jorge.hernan.esparza@gmail.com>
2019-07-20 17:48:39 +0200

    Added first part form

--- public/js/app.js
+++ public/js/app.js
@@ -18,6 +18,18 @@ new Vue({
         'partner_address_2': '',
         'partner_town_2': '',
         'partner_id_2': '',
+        'marriage_type': '',
+        'marriage_place': '',
+        'marriage_date': '',
+        'registry_place': '',
+        'registry_volume': '',
+        'registry_page': '',
+        'economic_system': '',
+        'children_number': '',
+        'last_partner_address': '',
+        'last_partner_town': '',
+        'family_house_address': '',
+        'family_house_town': ''
     },
     delimiters: ['${', '}']
 });
